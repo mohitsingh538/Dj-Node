6fdaa4a9975b0b797808dfa706568d293e6c5c32
Mohit Singh <mohitsingh538@gmail.com>
2021-10-31 22:39:18 +0530

    Added project name

--- files/home/urls.js
+++ files/home/urls.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/', function(req, res){
-    res.render(path.join(__dirname, "templates/home.ejs"))
+    const pkg_file = fs.readFileSync('package.json');
+    const proj_name = JSON.parse(pkg_file);
+    res.render(path.join(__dirname, "templates/home.ejs"), {projName: proj_name.name})
 });
 
 module.exports = router;
