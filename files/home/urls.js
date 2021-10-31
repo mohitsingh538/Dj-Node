aef0ec127dfa7f8faccd16f798f3d17b3823b78c
Mohit Singh <mohitsingh538@gmail.com>
2021-11-01 00:01:05 +0530

    Added Sign-in Page & Error 404 Page

--- files/home/urls.js
+++ files/home/urls.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/', function(req, res){
-    const pkg_file = fs.readFileSync('package.json');
-    const proj_name = JSON.parse(pkg_file);
+    var pkg_file = fs.readFileSync('package.json');
+    var proj_name = JSON.parse(pkg_file);
     res.render(path.join(__dirname, "templates/home.ejs"), {projName: proj_name.name})
 });
 
--- files/users/urls.js
+++ files/users/urls.js
@@ -3,7 +3,7 @@ const path = require("path");
 const router = express.Router();
 
 router.get('/sign-in', function(req, res){
-    res.render(path.join(__dirname, "templates/sign-in.ejs"))
+    res.render(path.join(__dirname, "templates/sign-in"))
 });
 
 module.exports = router;
